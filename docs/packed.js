699f593104897a3e3946466d7a190eec5cd36768
jeremybailey <baileyorama@gmail.com>
2024-02-11 20:34:25 -0700

    lens 2

--- docs/packed.js
+++ docs/packed.js
@@ -26030,7 +26030,7 @@ console.info(`SDK: ${environment_namespaceObject.l} \
 
     const { lenses } = await cameraKit.lensRepository.loadLensGroups (['7dbc9fa1-1ecf-4946-8bf4-ca8f7eee30c5'])
 
-    session.applyLens (lenses[1])
+    session.applyLens (lenses[2])
     let mediaStream = await navigator. mediaDevices.getUserMedia ({ video: true });
 
     const source = createMediaStreamSource(mediaStream, {
